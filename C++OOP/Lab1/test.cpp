9eb3ee8288bf6d2cacdc1cbc45bcb79f4c4a5307
Cussonspoon <140087747+cussonspoon@users.noreply.github.com>
2024-03-20 23:17:22 +0700

    Deleted excess folders

--- C++OOP/Lab1/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    cout << "This is my first program of C++" << endl;
-    cout << "Hello World!" << endl;
-    cout << "Good Bye!" << endl;  
-    return 0;
-}
